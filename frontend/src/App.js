aa8c8be357897b0299ddab91b4f33af3e39ff703
Elmer <contact@elmer.dev>
2022-10-01 15:03:53 -0700

    file structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
+import Dashboard from 'components/Dashboard/Dashboard';
+import Calendar from 'components/Calendar/Calendar';
+import Connections from 'components/Connections/Connections';
+import Search from 'components/Search/Search';
+import Settings from 'components/Settings/Settings';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Dashboard />
+            <Calendar />
+            <Connections />
+            <Search />
+            <Settings />
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/Connections/Connections.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Connections = () => {
+  return (
+    <div>Connections</div>
+  )
+}
+
+export default Connections
