81e3b9deb32b747e826db62b48a3563fbc32c509
Elmer <contact@elmer.dev>
2022-10-02 11:21:20 -0700

    folder restructure and package json updates

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json({ limit: '50mb' }));
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
