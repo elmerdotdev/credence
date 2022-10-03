30f753a50d1ce8a0c550a54c4c57dc321537b81f
Elmer Balbin Jr <contact@elmer.dev>
2022-10-03 00:55:36 -0700

    js and css file structure

--- frontend/src/pages/Connections/Connections.js
+++ frontend/src/pages/Connections/Connections.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Connections = () => {
   return (
-    <div>Connections</div>
+    <section className="page-connections">Connections</section>
   )
 }
 
--- frontend/src/pages/Settings/Settings.js
+++ frontend/src/pages/Settings/Settings.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <section className="page-settings">Settings</section>
   )
 }
 
