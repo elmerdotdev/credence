546eb042d2da27dc09f0ccfe17e3e03401ffdff6
Iris Chen <jaminitaa0322@gmail.com>
2022-10-15 02:29:51 -0700

    add modal, add component files

---
+++ frontend/src/pages/Connections/ClientCards.js
@@ -0,0 +1,11 @@
+import ClientCard from './ClientCard'
+
+const ClientCards = ({ task }) => {
+    return (
+       
+        <ClientCard  />
+
+    )
+}
+
+export default ClientCards
--- frontend/src/pages/Connections/Connections.js
+++ frontend/src/pages/Connections/Connections.js
@@ -1,18 +1,34 @@
 // insert一个新的client进database
 import React from 'react';
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import ClientCards from './ClientCards';
+import Modal from './Modal';
 
 const Connections = () => {
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [active, setActive] = useState(false);
-  const [user_id, setUserId] = useState('');
 
+  const [connections, setConnections] = useState([]);
+  const [showAddConnection, setShowAddConnection] = useState(false);
 
-const addClient = async (newClient) => {
-  
+  useEffect(() => {
+    const getConnections = async () => {
+      const res = await fetchConnections();
+      setConnections(res);
+    };
+
+    getConnections ();
+  }, []);
+
+// Fetch Connections
+const fetchConnections = async () => {
+  const res = await fetch('http://localhost:5002/api/clients');
+  const data = await res.json();
+  console.log(setShowAddConnection);
+
+  return data;
+};
+
+// Add Connection
+const addConnection = async (newClient) => {
   const res = await fetch('http://localhost:5002/api/clients', {
     method: 'POST',
     headers: {
@@ -22,97 +38,54 @@ const addClient = async (newClient) => {
   });
 
   const data = await res.json();
+  setConnections([...connections, data]);
 };
-const deleteClient = async () => {
+
+const deleteConnection = async () => {
   // const addClient = async (task) => {
 
   const res = await fetch('http://localhost:5002/api/clients/6348a9d86a8ff1e1f8253d67', {
-    method: 'DELETE',
+    method: 'DELETE'
   });
 
-  const data = await res.json();
+  // const data = await res.json();
 };
 
-const onSubmit = (e) => {
-  e.preventDefault();
+// const addClient = async (newClient) => {
+  
+//   const res = await fetch('http://localhost:5002/api/clients', {
+//     method: 'POST',
+//     headers: {
+//       'Content-type': 'application/json',
+//     },
+//     body: JSON.stringify(newClient),
+//   });
 
-  addClient({ firstname, lastname, email, phone, active, user_id });
+//   const data = await res.json();
+// };
+const modal_styles = {
+  position:'relative',
+  zIndex:1
+}
 
-  setFirstname('');
-  setLastname('');
-  setEmail('');
-  setPhone('');
-  setActive(false);
-  setUserId('')
-};
+const background_styles ={
+  position:'relative',
+  zIndex:2,
+}
+
+const[openModal,setOpenModal] = useState(false)
 
   return (
     <div>
-    <section className="page-connections">Connections</section>
-    <form className="add-form" onSubmit={onSubmit}>
-      <div className="form-control">
-        <label></label>
-        <input
-          type="text"
-          placeholder="First name"
-          value={firstname}
-          onChange={(e) => setFirstname(e.target.value)}
-        />
-      </div>
-      <div className="form-control">
-        <label></label>
-        <input
-          type="text"
-          placeholder="Last name"
-          value={lastname}
-          onChange={(e) => setLastname(e.target.value)}
-        />
-      </div>
-      <div className="form-control">
-        <label></label>
-        <input
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div className="form-control">
-        <label></label>
-        <input
-          type="text"
-          placeholder="Phone"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-        />
-      </div>
-      <div className="form-control form-control-check">
-        <label htmlFor="activeChkbox">Active</label>
-        <input
-          id="activeChkbox"
-          type="checkbox"
-          checked={active}
-          value={active}
-          onChange={(e) => setActive(e.currentTarget.checked)}
-        />
-      </div>
-      <div className="form-control">
-        <label></label>
-        <input
-          type="text"
-          placeholder="UserId"
-          value={user_id}
-          onChange={(e) => setUserId(e.target.value)}
-        />
-      </div>
-
-      <input type="submit" value="Save Client" className="btn btn-block" />
-    </form>
-  
-    <button onClick = {deleteClient}>del</button>
-
+      <section className="page-connections" style={background_styles}>Connections</section>
+      {/* <Modal open={isOpen} onClose={() => setIsOpen(false)}> Fancy Modal </Modal>  */}
+        <button className="openModalBtn" onClick={() => setOpenModal(true)}>Add New Client</button>
+      {/* <ClientCards />
+      <button onClick = {deleteConnection}>del</button> */}
+    {openModal && <Modal closeModal={setOpenModal}/>}
     </div>
   )
 }
+
 
 export default Connections
