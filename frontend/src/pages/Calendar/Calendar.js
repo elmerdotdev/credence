2d1bafa7dde045e80319f09b9f7ba85f9e0f6a83
Elmer Balbin Jr <contact@elmer.dev>
2022-10-20 23:04:49 -0700

    calendar updates

--- frontend/src/pages/Calendar/Calendar.js
+++ frontend/src/pages/Calendar/Calendar.js
@@ -2,28 +2,15 @@ import React, { useState, useEffect } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import Modal from 'react-modal'
 import moment from 'moment'
-import "react-datetime/css/react-datetime.css";
 
 // Components
 import EventsWidget from './components/EventsWidget'
-import AddEvent from './components/AddEvent'
-import ViewEvent from './components/ViewEvent'
 
 const Calendar = () => {
   const [events, setEvents] = useState([])
   const [currentMonth, setCurrentMonth] = useState('')
-  const [monthFirstDay, setMonthFirstDay] = useState()
-  const [monthLastDay, setMonthLastDay] = useState()
-  const [addDate, setAddDate] = useState()
-  const [viewEventId, setViewEventId] = useState('')
-
-  // Modal states
-  const [modalAddOpen, setModalAddOpen] = useState(false)
-  const [modalEditOpen, setModalEditOpen] = useState(false)
-  const [modalViewOpen, setModalViewOpen] = useState(false)
-
-  const calendarRef = React.useRef()
 
   const userID = '633b6a81145c9d79405c54ea'
 
@@ -39,72 +26,34 @@ const Calendar = () => {
 
   // Get all activities/events
   const fetchActivities = async () => {
-    const res = await fetch(`${process.env.REACT_APP_API_URL}/api/activities/${userID}`)
+    const res = await fetch(`http://localhost:5000/api/activities/${userID}`)
     const data = await res.json()
 
     return data
-  }
-
-  // Delete activity/event
-  const deleteEvent = async (id) => {
-    await fetch(`${process.env.REACT_APP_API_URL}/api/activities/${id}`, {
-      method: 'DELETE'
-    })
-
-    setEvents(events.filter(event => event._id !== id))
-    toggleViewModal(false)
-  }
-
-  // Get client
-  const fetchClient = async (id) => {
-    const res = await fetch(`${process.env.REACT_APP_API_URL}/api/clients/${userID}/${id}`)
-    const data = await res.json()
-
-    return data
-  }
-
-  // Toggle Add modal
-  const toggleAddModal = (status) => {
-    setModalAddOpen(status)
-  }
-
-  // Toggle View modal
-  const toggleViewModal = (status) => {
-    setModalViewOpen(status)
   }
   
   // On Date Cell Click
   const handleDateClick = (element) => {
-    toggleAddModal(true)
-    setAddDate(element.date)
+    console.log(element)
   }
 
   // On Event Click
   const handleEventClick = (element) => {
-    toggleViewModal(true)
-    setViewEventId(element.event._def.publicId)
+    console.log(element)
+    console.log(element.event._def.title)
   }
 
   // Run when Calendar View loads or changes
   const handleCalendarLoad = (arg) => {
-    setCurrentMonth(moment(arg.start).format("MMMM YYYY"))
-    setMonthFirstDay(moment(arg.start).format("YYYY-MM-DD"))
-    setMonthLastDay(moment(arg.end).subtract(1, "days").format("YYYY-MM-DD"))
-  }
-
-  // Add New Event to events state
-  const addToEventsState = (event) => {
-    setEvents(current => [...current, event])
+    const month = moment(arg.start).format("MMMM")
+    setCurrentMonth(month)
   }
 
   return (
     <section className="page-calendar">
 
       <div className="page-calendar-view">
-        <button onClick={() => toggleAddModal(true)}>Add Event</button>
-
       <FullCalendar
-        ref={calendarRef}
         plugins={[ dayGridPlugin, interactionPlugin ]}
         initialView="dayGridMonth"
         events={
@@ -124,11 +73,7 @@ const Calendar = () => {
       />
       </div>
 
-      <EventsWidget events={events} currMonth={currentMonth} firstDay={monthFirstDay} lastDay={monthLastDay} onEventClick={handleEventClick} fetchClient={fetchClient} />
-
-      <AddEvent modalOpen={modalAddOpen} onToggle={toggleAddModal} onDateClick={addDate} onAddState={addToEventsState} userId={userID} />
-
-      <ViewEvent modalOpen={modalViewOpen} onToggle={toggleViewModal} onDelete={deleteEvent} userId={userID} eventId={viewEventId} fetchClient={fetchClient} />
+      <EventsWidget currMonth={currentMonth} events={events} />
 
     </section>
   )
--- frontend/src/pages/Calendar/components/EventsWidget.js
+++ frontend/src/pages/Calendar/components/EventsWidget.js
@@ -1,59 +1,14 @@
-import React, { useState, useEffect } from 'react'
-import moment from 'moment'
-import EventsWidgetRow from './EventsWidgetRow'
+import React from 'react'
 
 const EventsWidget = (props) => {
-    const [ events, setEvents ] = useState([])
-    const [ groupedEvents, setGroupedEvents ] = useState([])
+  return (
+    
+    <div className="page-calendar-widget">
+        <h2>{props.currMonth}</h2>
+        <p>There are {props.events.length} events bro.</p>
+    </div>
 
-    useEffect(() => {
-        // Filter events to only show current month
-        const monthEvents = props.events.filter(d => {
-            return (props.firstDay < d.start_date && d.start_date < props.lastDay)
-        }).sort((a, b) => a.start_date.localeCompare(b.start_date))
-
-        setEvents(monthEvents)
-
-        // Group by day
-        const groups = monthEvents.reduce((groups, day) => {
-            const date = moment(day.start_date).format("YYYY-MM-DD")
-            if (!groups[date]) {
-                groups[date] = [];
-            }
-            groups[date].push(day);
-            return groups;
-        }, {});
-        
-        const groupArrays = Object.keys(groups).map((date) => {
-            return {
-                date,
-                events: groups[date]
-            };
-        });
-
-        setGroupedEvents(groupArrays)
-    }, [ props.events, props.firstDay, props.lastDay])
-
-    return (
-        <div className="page-calendar-widget">
-            <h2>{props.currMonth}</h2>
-
-            {events.length === 0 ? (
-                <button>Add Your First Event</button>
-            ) : (
-                <div className="monthlyEvents">
-                    {groupedEvents.map((day, i) => (
-                        <div className="dateEvents" key={i}>
-                        <h3>{moment(day.date).format("DD")}</h3>
-                        {day.events.map((event, i) => (
-                            <EventsWidgetRow event={event} key={i} onEventClick={props.onEventClick} fetchClient={props.fetchClient} />
-                        ))}
-                    </div>
-                    ))}
-                </div>
-            )}
-        </div>
-    )
+  )
 }
 
 export default EventsWidget
