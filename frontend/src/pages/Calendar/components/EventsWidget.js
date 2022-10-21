b4d700f09084579f235a480fa2e0472605e7a16c
Elmer Balbin Jr <contact@elmer.dev>
2022-10-20 23:09:10 -0700

    calendar updates

--- frontend/src/pages/Calendar/components/EventsWidget.js
+++ frontend/src/pages/Calendar/components/EventsWidget.js
@@ -6,6 +6,10 @@ const EventsWidget = (props) => {
     <div className="page-calendar-widget">
         <h2>{props.currMonth}</h2>
         <p>There are {props.events.length} events bro.</p>
+        
+        {props.events.length === 0 && (
+            <button>Add Event</button>
+        )}
     </div>
 
   )
