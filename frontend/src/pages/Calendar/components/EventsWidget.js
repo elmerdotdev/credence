892bcf4c956c4a9fcd52d3a734aa5b103efb9975
Elmer Balbin Jr <contact@elmer.dev>
2022-10-21 01:11:21 -0700

    calendar updates

--- frontend/src/pages/Calendar/components/EventsWidget.js
+++ frontend/src/pages/Calendar/components/EventsWidget.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
 
 const EventsWidget = (props) => {
-  return (
-    
-    <div className="page-calendar-widget">
-        <h2>{props.currMonth}</h2>
-        <p>There are {props.events.length} events bro.</p>
-        
-        {props.events.length === 0 && (
-            <button>Add Event</button>
-        )}
-    </div>
+    const [ events, setEvents ] = useState([])
 
-  )
+    useEffect(() => {
+        // Filter events to only show current month
+        const monthEvents = props.events.filter(d => {
+            return (props.firstDay < d.start_date && d.start_date < props.lastDay)
+        })
+
+        setEvents(monthEvents)
+    }, [ props.events, props.firstDay, props.lastDay ])
+
+    return (
+        <div className="page-calendar-widget">
+            <h2>{props.currMonth}</h2>
+
+            {events.length === 0 ? (
+                <button>Add Your First Event</button>
+            ) : (
+                <ul>
+                    {events.map(event => {
+                        return eventListItem(event)
+                    })}
+                </ul>
+            )}
+        </div>
+    )
+}
+
+const eventListItem = (event) => {
+    return (
+        <li key={event._id}>
+            <div>{event.title}</div>
+            <div>Start: {moment(event.start_date).format("hh:mm A")}</div>
+            <div>End: {moment(event.end_date).format("hh:mm A")}</div>
+        </li>
+    )
 }
 
 export default EventsWidget
