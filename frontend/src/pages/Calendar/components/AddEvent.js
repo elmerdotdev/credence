27d7c680dbd533184cd979f456f0bd61b38a9413
Elmer Balbin Jr <contact@elmer.dev>
2022-10-21 18:03:51 -0700

    more updates

--- frontend/src/pages/Calendar/components/AddEvent.js
+++ frontend/src/pages/Calendar/components/AddEvent.js
@@ -21,16 +21,33 @@ const AddEvent = (props) => {
     const [start, setStart] = useState()
     const [end, setEnd] = useState()
     const [type, setType] = useState('')
+    const [clients, setClients] = useState([])
     const [clientId, setClientId] = useState()
     
     useEffect(() => {
+        const getClients = async () => {
+            const res = await fetch(`http://localhost:5000/api/clients/${props.userId}`)
+            const data = await res.json()
 
-    }, [])
+            console.log(data)
+
+            setClients(data)
+        }
+
+        getClients()
+    }, [ props.userId, props.modalOpen ])
+
+    // const fetchClients = async () => {
+    //     const res = await fetch(`http://localhost:5000/api/clients/${props.userId}`)
+    //     const data = await res.json()
+
+    //     return data
+    // }
 
     const submitForm = (event) => {
         event.preventDefault()
 
-        alert('1')
+        console.log(title, description, start, end, type, clientId)
     }
 
     return (
@@ -50,22 +67,36 @@ const AddEvent = (props) => {
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="description">Description</label>
-                        <textarea id="description" cols="30" rows="10"></textarea>
+                        <textarea id="description" cols="30" rows="10" value={description} onChange={e => setDescription(e.target.value)} />
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="startdate">Start</label>
-                        <Datetime value={start} onChange={date => setStart(date)} />
+                        <Datetime initialValue={props.onDateClick} onChange={date => setStart(date)} />
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="enddate">End</label>
-                        <Datetime value={start} onChange={date => setEnd(date)} />
+                        <Datetime value={end} onChange={date => setEnd(date)} />
                     </div>
                     <div className="input-wrapper">
-                        <select>
-
+                        <label htmlFor="eventtype">Event Type</label>
+                        <select id="eventtype" defaultValue={type || ''} onChange={e => setType(e.target.value) || ''}>
+                            <option value=""></option>
+                            <option value="meeting">Meeting</option>
+                            <option value="birthday">Birthday</option>
                         </select>
                     </div>
-                    <input type="submit" value="Add New Event" />
+                    <div className="input-wrapper">
+                        <label htmlFor="clientlist">Client</label>
+                        <select id="clientlist" value={clientId || ''} onChange={e => setClientId(e.target.value) || ''}>
+                            <option value=""></option>
+                            {clients.map((client, i) => (
+                                <option key={i} value={client._id}>{client.firstname} {client.lastname}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="input-wrapper">
+                        <input type="submit" value="Add New Event" />
+                    </div>
                 </form>
             </Modal>
         </div>
