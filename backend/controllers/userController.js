9ce97066d5c82902322fdcbc950b33f67cea5b7e
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2022-10-26 01:53:22 -0700

    password update, login and signup page move ok

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
-const { OAuth2Client } = require('google-auth-library')
+// const { OAuth2Client } = require('google-auth-library')
 
 // Get a single user
 const getUser = async (req, res) => {
--- frontend/src/pages/Signup/hooks/useSignup.js
+++ frontend/src/pages/Signup/hooks/useSignup.js
@@ -10,7 +10,7 @@ export const useSignup = () => {
         setIsLoading(true)
         setError(null)
 
-        const res = await fetch('http://localhost:5000/api/users/login', {
+        const res = await fetch('http://localhost:5000/api/users/signup', {
             method:'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({firstname, lastname, email, password})
