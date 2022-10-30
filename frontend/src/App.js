6dc3a007a41442a792b0f8d46cfebb88c5b2896a
Elmer Balbin Jr <contact@elmer.dev>
2022-10-30 01:59:32 -0700

    css and sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,13 @@ import Notes from './pages/Notes/Notes';
 import Profile from './pages/Profile/Profile';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
+
+// Landing Pages
+import Home from './pages/Landing/Home/Home';
+import Features from './pages/Landing/Features/Features';
+import Pricing from './pages/Landing/Pricing/Pricing';
+import About from './pages/Landing/About/About';
+
 import './App.css';
 import logo from './images/logo.svg';
 
@@ -18,18 +25,47 @@ function App() {
     return (
         <BrowserRouter>
         <div className="App">
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/connections" element={<Connections />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/settings" element={<Settings />} />
-                <Route path="/notes" element={<Notes />} />
-                <Route path ="/notes/:client_id/:id" element={<ViewNote />} />
-                <Route path = "notes/edit/:client_id/:id" element={<EditNote />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-            </Routes>
+            <header>
+                <div className="logo">
+                    <img src={logo} alt="Credence Logo" />
+                </div>
+                <div className="search">
+                    <span>Search</span>
+                </div>
+            </header>
+            <section className="container">
+                <aside>
+                    <nav className="App-navigation">
+                        <ul>
+                            <li><Link to="/dashboard">Dashboard</Link></li>
+                            <li><Link to="/connections">Connection</Link></li>
+                            <li><Link to="/calendar">Calendar</Link></li>
+                            <li><Link to="/profile">Profile</Link></li>
+                        </ul>
+                        <ul>
+                            <li><Link to="/settings">Settings</Link></li>
+                            <li><Link to="/logout">Log Out</Link></li>
+                        </ul>
+                    </nav>
+                </aside>
+                <div className="App-body">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/features" element={<Features />} />
+                        <Route path="/pricing" element={<Pricing />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/calendar" element={<Calendar />} />
+                        <Route path="/connections" element={<Connections />} />
+                        <Route path="/search" element={<Search />} />
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/settings" element={<Settings />} />
+                        <Route path="/notes" element={<Notes />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                    </Routes>
+                </div>
+            </section>
         </div>
         </BrowserRouter>
     );
