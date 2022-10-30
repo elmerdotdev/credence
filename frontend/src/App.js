7c38c89aad45b6f5c44fff4630152e395c6960e6
Elmer Balbin Jr <contact@elmer.dev>
2022-10-30 01:59:32 -0700

    css and sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-// Pages
+// App pages
 import Dashboard from './pages/Dashboard/Dashboard';
 import Calendar from './pages/Calendar/Calendar';
 import Connections from './pages/Connections/Connections';
 import Search from './pages/Search/Search';
 import Settings from './pages/Settings/Settings';
 import Notes from './pages/Notes/Notes';
+import Profile from './pages/Profile/Profile';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 
+// Landing Pages
+import Home from './pages/Landing/Home/Home';
+import Features from './pages/Landing/Features/Features';
+import Pricing from './pages/Landing/Pricing/Pricing';
+import About from './pages/Landing/About/About';
+
 import './App.css';
+import logo from './images/logo.svg';
 
 function App() {
     return (
@@ -18,7 +27,7 @@ function App() {
         <div className="App">
             <header>
                 <div className="logo">
-                    <h1>Logo</h1>
+                    <img src={logo} alt="Credence Logo" />
                 </div>
                 <div className="search">
                     <span>Search</span>
@@ -26,9 +35,9 @@ function App() {
             </header>
             <section className="container">
                 <aside>
-                    <nav>
+                    <nav className="App-navigation">
                         <ul>
-                            <li><Link to="/">Dashboard</Link></li>
+                            <li><Link to="/dashboard">Dashboard</Link></li>
                             <li><Link to="/connections">Connection</Link></li>
                             <li><Link to="/calendar">Calendar</Link></li>
                             <li><Link to="/profile">Profile</Link></li>
@@ -39,16 +48,21 @@ function App() {
                         </ul>
                     </nav>
                 </aside>
-                <div class="App-body">
+                <div className="App-body">
                     <Routes>
-                        <Route key="1" path="/" element={<Dashboard />} />
-                        <Route key="2" path="/calendar" element={<Calendar />} />
-                        <Route key="3" path="/connections" element={<Connections />} />
-                        <Route key="4" path="/search" element={<Search />} />
-                        <Route key="5" path="/settings" element={<Settings />} />
-                        <Route key="6" path="/notes" element={<Notes />} />
-                        <Route key="7" path="/login" element={<Login />} />
-                        <Route key="8" path="/signup" element={<Signup />} />
+                        <Route path="/" element={<Home />} />
+                        <Route path="/features" element={<Features />} />
+                        <Route path="/pricing" element={<Pricing />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/calendar" element={<Calendar />} />
+                        <Route path="/connections" element={<Connections />} />
+                        <Route path="/search" element={<Search />} />
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/settings" element={<Settings />} />
+                        <Route path="/notes" element={<Notes />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
                     </Routes>
                 </div>
             </section>
