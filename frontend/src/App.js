1874a73c5c4b45be6f6e3b0f3a881903f345cddf
Danika Enad <danika.enad@gmail.com>
2022-10-31 21:48:38 -0700

    updated app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,6 @@ function App() {
     return (
         <BrowserRouter>
         <div className="App">
-<<<<<<< HEAD
             <Header />
             <section className="container">
                 <aside>
@@ -64,18 +63,6 @@ function App() {
                     </Routes>
                 </div>
             </section>
-=======
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/connections" element={<Connections />} />
-                <Route path="/search" element={<Search />} />
-                <Route path="/settings" element={<Settings />} />
-                <Route path="/notes" element={<Notes />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-            </Routes>
->>>>>>> 29a4359 (added modals for notes)
         </div>
         </BrowserRouter>
     );
