5d6ad68b75f5f9de624c104e8d1c7c7ae7707c28
Elmer Balbin Jr <contact@elmer.dev>
2022-11-02 22:07:08 -0700

    fontello update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,7 @@ function App() {
                 <aside>
                     <nav className="App-navigation">
                         <ul>
-                            <li><i className="icon-dashboard"></i> <NavLink to="/dashboard">Dashboard</NavLink></li>
+                            <li><i className="icon-home"></i> <NavLink to="/dashboard">Dashboard</NavLink></li>
                             <li><i className="icon-connection"></i> <NavLink to="/connections">Connection</NavLink></li>
                             <li><i className="icon-calendar"></i> <NavLink to="/calendar">Calendar</NavLink></li>
                             <li><i className="icon-help"></i><NavLink to="/reports">Reports</NavLink></li>
