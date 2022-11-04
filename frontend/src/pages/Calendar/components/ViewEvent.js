a90f5a58091604b13a09a0a3987b23831713d9d8
Elmer Balbin Jr <contact@elmer.dev>
2022-11-03 23:46:18 -0700

    credence modal

--- frontend/src/pages/Calendar/components/ViewEvent.js
+++ frontend/src/pages/Calendar/components/ViewEvent.js
@@ -39,42 +39,45 @@ const ViewEvent = (props) => {
                         <i className="icon-close" onClick={() => props.onToggle(false)}></i>
                     </div>
                     <div>
-                        <button>Edit</button>
-                        <button onClick={() => props.onDelete(props.eventId)}>Delete</button>
+                        <button className="btn btn-primary-reverse">Edit <i className="icon-edit"></i></button>
+                        <button className="btn btn-primary-reverse" onClick={() => props.onDelete(props.eventId)}>Delete <i className="icon-trash"></i></button>
                     </div>
                 </div>
-                <h2>{event.title}</h2>
-                <table className="viewModalTable">
-                    <tbody>
-                        <tr>
-                            <th>Category</th>
-                            <td>{event.type}</td>
-                        </tr>
-                        <tr>
-                            <th>Date</th>
-                            <td>
-                                {moment(event.start_date).format("DD") !== moment(event.end_date).format("DD") ? (
-                                    <div className="viewEventDates">
-                                        <span>{moment(event.start_date).format("DD MMM YYYY, hh:mm a")}</span>
-                                        <span>{moment(event.end_date).format("DD MMM YYYY, hh:mm a")}</span>
-                                    </div>
-                                ) : (
-                                    <span>{moment(event.start_date).format("DD MMM YYYY, hh:mm a")} - {moment(event.end_date).format("hh:mm a")}</span>
-                                )}
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>Client</th>
-                            <td>{clientName}</td>
-                        </tr>
-                        <tr>
-                            <th colSpan="2">Description</th>
-                        </tr>
-                        <tr>
-                            <td colSpan="2">{event.description}</td>
-                        </tr>
-                    </tbody>
-                </table>
+
+                <div class="viewModalContent">
+                    <h2>{event.title}</h2>
+                    <table className="viewModalTable">
+                        <tbody>
+                            <tr>
+                                <th>Category</th>
+                                <td>{event.type}</td>
+                            </tr>
+                            <tr>
+                                <th>Date</th>
+                                <td>
+                                    {moment(event.start_date).format("DD") !== moment(event.end_date).format("DD") ? (
+                                        <div className="viewEventDates">
+                                            <span>{moment(event.start_date).format("DD MMM YYYY, hh:mm a")}</span>
+                                            <span>{moment(event.end_date).format("DD MMM YYYY, hh:mm a")}</span>
+                                        </div>
+                                    ) : (
+                                        <span>{moment(event.start_date).format("DD MMM YYYY, hh:mm a")} - {moment(event.end_date).format("hh:mm a")}</span>
+                                    )}
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>Client</th>
+                                <td>{clientName}</td>
+                            </tr>
+                            <tr>
+                                <th colSpan="2">Description</th>
+                            </tr>
+                            <tr>
+                                <td colSpan="2">{event.description}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </Modal>
         </div>
     )
