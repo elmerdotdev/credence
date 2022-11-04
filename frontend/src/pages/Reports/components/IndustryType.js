a54696e41603b45da4f09693f2364d35b0361893
Danika Enad <danika.enad@gmail.com>
2022-11-04 14:41:45 -0700

    updated userID for fetch URL

--- frontend/src/pages/Reports/components/IndustryType.js
+++ frontend/src/pages/Reports/components/IndustryType.js
@@ -32,7 +32,7 @@ const IndustryType = () => {
   }, [])
 
     const fetchClients = async () => {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/clients/633b6a81145c9d79405c54ea`)
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/clients/63645e4850049bfd1e89637a`)
       const data = await response.json()
 
       if (response.ok) {
