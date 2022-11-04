f8cd53e120f2c8e000ae54ff3a0c432f57e801ce
Elmer Balbin Jr <contact@elmer.dev>
2022-11-03 22:46:55 -0700

    credence modal

--- frontend/src/pages/Calendar/components/ViewEvent.js
+++ frontend/src/pages/Calendar/components/ViewEvent.js
@@ -2,17 +2,6 @@ import React, { useState, useEffect } from 'react'
 import Modal from 'react-modal'
 import moment from 'moment'
 
-const customStyles = {
-    content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-    },
-};
-
 Modal.setAppElement('body');
 
 const ViewEvent = (props) => {
