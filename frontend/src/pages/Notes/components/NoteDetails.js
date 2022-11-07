5471a6cf2b143af8cada5843715aa672df4d7585
Danika Enad <danika.enad@gmail.com>
2022-11-07 12:23:33 -0800

    made view notes only show for each specific client

--- frontend/src/pages/Notes/components/NoteDetails.js
+++ frontend/src/pages/Notes/components/NoteDetails.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AddNote from './AddNote'
 import NoteList from './NoteList'
 
-const NoteDetails = ({ notes, onAdd, viewNote, noteId }) => {
-   
+const NoteDetails = ({notes, onAdd, viewNote, noteId }) => {
+    // const [allClientNotes, setallClientNotes] = useState(null)
+
+    // useEffect(() => {
+
+    //     const getNotes = async () => {
+    //       const clientNotes = await fetchNotes();
+    //       const viewClientNotes = clientNotes.filter((clientNote) => {return clientNote.client_id})
+    //       console.log(viewClientNotes)
+
+    //       setallClientNotes(viewClientNotes);
+    //   };
+    
+    //     getNotes();
+    //   }, [])
+    
+        //Fetch All Notes For Specific Client
+        // const fetchNotes = async () => {
+        //   const response = await fetch(`${process.env.REACT_APP_API_URL}/api/notes/63645e4850049bfd1e89637a`)
+        //   const data = await response.json()
+    
+        //   if (response.ok) {
+        //       return data
+        //   }
+        // }
+
     return (
         <div>
             <section className = "add-note">
