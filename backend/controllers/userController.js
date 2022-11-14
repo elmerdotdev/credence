76a49b44887d83e98baeba25ccd4edf4a5e8b8a7
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2022-11-13 22:02:52 -0800

    lastloggedin in loginuser is added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
-// const { OAuth2Client } = require('google-auth-library')
 
 // Get a single user
 const getUser = async (req, res) => {
