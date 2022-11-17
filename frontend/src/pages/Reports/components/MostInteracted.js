726ff09213370fa5d80622b7c10f84014345107e
Danika Enad <danika.enad@gmail.com>
2022-11-17 11:35:56 -0800

    updating data on mostinteracted chart

--- frontend/src/pages/Reports/components/MostInteracted.js
+++ frontend/src/pages/Reports/components/MostInteracted.js
@@ -8,42 +8,74 @@ ChartJS.register(
     ArcElement
 )
 const MostInteracted = () => {
-    // const [activeClients, setActiveClients] = useState('')
-    // const [inactiveClients, setInactiveClients] = useState('')
+    const [allClients, setAllClients] = useState('')
+    const [notes, setNotes] = useState('')
+    const [dateCreated, setDateCreated] = useState('')
+    const [phone, setPhone] = useState('')
 
-    // useEffect(() => {
-    //     const getClients = async () => {
-    //         const clients = await fetchClients();
-    //         const active = clients.filter((client) => client.active == true)
-    //         const inactive = clients.filter((client) => client.active == false)
-    //         setActiveClients(active.length)
-    //         setInactiveClients(inactive.length)
-    //     }
+    useEffect(() => {
+        const getClients = async () => {
+            const clients = await fetchClients();
+            console.log(clients);
+            const created = clients.map(index => {
+                return index._id
+            });
+            const upd = clients.map(index => {
+                return index.phone
+            });
 
-    //     getClients();
-    // }, [])
+            setAllClients(clients);
+            setPhone(upd);
+            setDateCreated(created);
+            console.log(created);
 
-    // const fetchClients = async () => {
-    //     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/clients/63645e4850049bfd1e89637a`)
-    //     const data = await response.json()
+        }
 
-    //     if (response.ok) {
-    //         return data
-    //     }
-    // }
+        const getNotes = async () => {
+            const notes = await fetchNotes();
+            setNotes(notes);
+            console.log(notes);
+        }
+
+        getClients();
+        getNotes();
+    }, [])
+
+    const fetchClients = async () => {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/clients/63645e4850049bfd1e89637a`)
+        const data = await response.json()
+
+        if (response.ok) {
+            return data
+        }
+    }
+
+    const fetchNotes = async () => {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/notes/63645e4850049bfd1e89637a`)
+        const data = await response.json()
+
+        if (response.ok) {
+            return data
+        }
+    }
+
+    const xArray = dateCreated;
+    const yArray = phone - 1000;
+    const rArray = ["5", "6", "7", "9", "5", "5", "7", "10", "4", "3", "5", "6", "7", "9", "5", "5", "7", "10", "4", "3"]
+    const chartData = [];
+
+    xArray.forEach(function(e, i) {
+        chartData.push({
+            x: parseFloat(e),
+            y: parseFloat(yArray[i]),
+            r: parseFloat(rArray[i]),
+        });
+    });
 
     const data = {
         datasets: [{
           label: 'First Dataset',
-          data: [{
-            x: 20,
-            y: 30,
-            r: 15
-          }, {
-            x: 40,
-            y: 10,
-            r: 10
-          }],
+          data: chartData,
           backgroundColor: 'rgb(255, 99, 132)'
         }]
       };
