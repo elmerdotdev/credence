a56ebbc5f40d4d2c63187a1c02bb057b9275ea6b
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2022-11-17 21:22:32 -0800

    edit profile css and code update

--- frontend/src/pages/Settings/EditProfile.js
+++ frontend/src/pages/Settings/EditProfile.js
@@ -15,7 +15,7 @@ const EditProfile = (props) => {
 
     //get userID from Localstorage(user ID)
     const userID = JSON.parse(localStorage.getItem('user'))._id
-    console.log(userID)
+    // console.log(userID)
 
     //page close in editModal
     const closepage = () => {
@@ -36,6 +36,7 @@ const EditProfile = (props) => {
         getProfile();
     },[])
     
+    console.log(photo)
     const fetchProfile = async () => {
       const res = await fetch(`${process.env.REACT_APP_API_URL}/api/users/${userID}`);
       const data = await res.json();  
@@ -69,10 +70,6 @@ const EditProfile = (props) => {
         }
     }
     
-    //Add new image
-    const [ file, setFile] = useState(null)
-
-
     //edit submit function
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -80,54 +77,79 @@ const EditProfile = (props) => {
         await editing(firstName, lastName, email, photo)
         console.log(`New Profile:` + firstName, lastName, email, photo)
 
-        setFirstName('')
-        setLastName('')
-        setEmail('')
+        // setFirstName('')
+        // setLastName('')
+        // setEmail('')
+    }
+    //picture upload
+    const photoUpdate = (e) => {
+        // const reader = new FileReader();
+        // reader.onload = res => {
+        //     console.log(res.target.result); // Print file contents
+        //   }
+        // reader.readAsText(e.target.files[0])    
+        
+        // console.log(photo)
+        const newphoto = URL.createObjectURL(e.target.files[0])
+       const slicing = JSON.stringify(newphoto.slice(5))
+       console.log(slicing)
+       setPhoto(newphoto)
     }
 
 
   return (
-    <div className='section-EditProfile'>
+    <div className='editProfile-modal'>
+        <h2>Edit Profile</h2>
         <form onSubmit={handleSubmit}>
-            <div className="Add-photo-area"  >
+            
+            <div className="add-photo-area" >
                 <img src={photo} alt="user-img" />
-                <p >+</p>
-                <input type="file" accept="image/*"  onChange={(e) => setPhoto(URL.createObjectURL(e.target.files[0]))} />
-                <p>Add New Image</p>
+                <div className='background'></div>
+                {/* <p >+</p> */}
+            </div>
+            <p >Add New Image</p>
+            <input type="file" accept="image/*"  onChange={photoUpdate} />
+
+            <div className="input-area">
+                <label htmlFor="firstname" >First Name / Nickname</label>
+                <input  type="text"
+                        htmlFor="firstname"
+                        placeholder={firstName}
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
+                />
+            </div>
+            <div className="input-area">
+                <label htmlFor="lastname">Last Name</label>
+                <input  type="text"
+                        htmlFor="lastname"
+                        placeholder={lastName}
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
+                />
+            </div>
+            <div className="input-area">
+                <label htmlFor="email">Email</label>
+                <input  type="email"
+                        htmlFor="email"
+                        placeholder={email}
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                />
             </div>
 
-            <label htmlFor="firstname" >First Name / Nickname</label>
-            <input  type="text" 
-                    htmlFor="firstname" 
-                    placeholder={firstName} 
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)} 
-            />
-            <label htmlFor="lastname">Last Name</label>
-            <input  type="text" 
-                    htmlFor="lastname" 
-                    placeholder={lastName} 
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)} 
-            />
-            <label htmlFor="email">Email</label>
-            <input  type="email" 
-                    htmlFor="email" 
-                    placeholder={email} 
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)} 
-            />
-
-            <button type="submit" onClick={() => setEditModal(true)}>Edit Profile</button>
+            <div className="edit-btn-area"><button type="submit" onClick={() => setEditModal(true)} className="edit-profile-btn btn btn-primary">Edit Profile</button></div>
         </form>
 
-            <Modal isOpen={editModal}>
-                <p>Your New Account</p>
+            <Modal isOpen={editModal} className="submitted-editprofile-modal">
+                <h2>Your New Account</h2>
                 <img src={photo} alt="new-userimg" />
-                <p>First Name/ Nickname: {firstName}</p>
-                <p>Last Name: {lastName}</p>
-                <p>Email: {email}</p>
-                <button onClick={closepage}>Close</button>
+                <div className="result-area">
+                    <p>First Name/ Nickname: {firstName}</p>
+                    <p>Last Name: {lastName}</p>
+                    <p>Email: {email}</p>
+                </div>
+                <button onClick={closepage} className="submitted-editprofile-btn btn btn-primary-reverse">Close</button>
             </Modal>
       
     </div>
