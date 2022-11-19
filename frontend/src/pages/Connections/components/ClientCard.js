ffc12ef707b61d098f21b2b6d1a90296342c7577
Elmer <contact@elmer.dev>
2022-11-18 22:11:11 -0800

    quick add

--- frontend/src/pages/Connections/components/ClientCard.js
+++ frontend/src/pages/Connections/components/ClientCard.js
@@ -3,13 +3,15 @@ const ClientCard = ({ connection, onToggle }) => {
 
     return (
       <div className="client-card" onClick = {() => onToggle(connection._id)}>
-        <div>
-          <div>
+        <div className="connection-details">
+          <div className="connection-details-photo">
             <img src={connection.photo} alt={connection.firstname} />
           </div>
-          <h4>{connection.firstname}{' '} {connection.lastname}{' '}</h4>
-          <p className="connection-position">{connection.position}</p>
-          <p className="connection-company">{connection.company}</p>
+          <div className="connection-details-text">
+            <h4>{connection.firstname}{' '} {connection.lastname}{' '}</h4>
+            <p className="connection-position">{connection.position}</p>
+            <p className="connection-company">{connection.company}</p>
+          </div>
         </div>
 
         <div className="connection-interaction">Last interaction: <strong>2 days ago</strong></div>
