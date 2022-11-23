23b7c70d01588a7144f8923d3ec12e559510a4bd
Danika Enad <danika.enad@gmail.com>
2022-11-23 10:49:15 -0800

    preliminary stacked bar chart created

--- frontend/src/pages/Reports/components/MostInteracted.js
+++ frontend/src/pages/Reports/components/MostInteracted.js
@@ -12,91 +12,99 @@ ChartJS.register(
   );
 
 const MostInteracted = () => {
-    const [notes, setNotes] = useState('')
-    const [events, setEvents] = useState('')
-    const [dateCreated, setDateCreated] = useState('')
-    const [phone, setPhone] = useState('')
+    const [technology, setTechnology] = useState('')
+    const [hospitality, setHospitality] = useState('')
+    const [finance, setFinance] = useState('')
+    const [retail, setRetail] = useState('')
+    const [artAndDesign, setArtAndDesign] = useState('')
+    const [manufacturing, setManufacturing] = useState('')
+    const [media, setMedia] = useState('')
 
-    // useEffect(() => {
-
-
-    //     const getNotes = async () => {
-    //         const notes = await fetchNotes();
-    //         // const notesByClient = notes.reduce((map, val) => {
-    //         //     if(!map[val.client_id]) {
-    //         //         map[val.client_id] = [];
-    //         //     }
-    //         //     map[val.client_id].push(val.client_id);
-    //         //     return map
-    //         // })
-    //         const notesByClient = notes.map()
-    //         setNotes(notes);
-    //         console.log(notesByClient)
-    //     }
-
-    //     const getEvents = async () => {
-    //         const events = await fetchEvents();
-    //         setEvents(events);
-    //         console.log(events);
-    //     }
-
-    //     getNotes();
-    //     getEvents();
-    // }, [])
+    const userID = JSON.parse(localStorage.getItem('user'))._id
 
 
-    // const fetchNotes = async () => {
-    //     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/notes/63645e4850049bfd1e89637a`)
-    //     const data = await response.json()
+    useEffect(() => {
+      const getClients = async () => {
+          const clients = await fetchClients();
+          const tech = clients.filter((client) => client.labels.some((label) => label.text === 'Technology' && label.select === true))
+          const hosp = clients.filter((client) => client.labels.some((label) => label.text === 'Hospitality' && label.select === true))
+          const fin = clients.filter((client) => client.labels.some((label) => label.text === 'Finance' && label.select === true))
+          const ret = clients.filter((client) => client.labels.some((label) => label.text === 'Retail' && label.select === true))
+          const art = clients.filter((client) => client.labels.some((label) => label.text === 'Art & Design' && label.select === true))
+          const manu = clients.filter((client) => client.labels.some((label) => label.text === 'Manufacturing' && label.select === true))
+          const med = clients.filter((client) => client.labels.some((label) => label.text === 'Media' && label.select === true))
+          
+          setTechnology(tech.length)
+          setHospitality(hosp.length)
+          setFinance(fin.length)
+          setRetail(ret.length)
+          setArtAndDesign(art.length)
+          setManufacturing(manu.length)
+          setMedia(med.length)
 
-    //     if (response.ok) {
-    //         return data
-    //     }
-    // }
+      }
 
-    // const fetchEvents = async () => {
-    //     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/activities/63645e4850049bfd1e89637a`)
-    //     const data = await response.json()
+      getClients();
+  }, [])
 
-    //     if (response.ok) {
-    //         return data
-    //     }
-    // }
+    const fetchClients = async () => {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/clients/${userID}`)
+      const data = await response.json()
 
-    // const xArray = dateCreated;
-    // const yArray = phone - 1000;
-    // const rArray = ["5", "6", "7", "9", "5", "5", "7", "10", "4", "3", "5", "6", "7", "9", "5", "5", "7", "10", "4", "3"]
-    // const chartData = [];
+      if (response.ok) {
+          return data
+      }
+  }
 
-    // xArray.forEach(function(e, i) {
-    //     chartData.push({
-    //         x: parseFloat(e),
-    //         y: parseFloat(yArray[i]),
-    //         r: parseFloat(rArray[i]),
-    //     });
-    // });
-
-    // const data = {
-    //     datasets: [{
-    //       label: 'First Dataset',
-    //       data: chartData,
-    //       backgroundColor: 'rgb(255, 99, 132)'
-    //     }]
-    //   };
-
-    // const options = {
-    //     plugins: {
-    //         legend: {
-    //             display: false,
-    //         }
-    //     }
-    // }
+    const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false,
+          },
+          tooltip: {
+            yAlign: 'bottom'
+          }
+        },
+        scales: {
+            x: {
+                stacked: true,
+                ticks: {
+                    display: false,
+                },
+            },
+            y: {
+                stacked: true,
+            },
+        },
+      };
+            
+    const data = {
+        labels:['Technology', 'Hospitality', 'Finance', 'Retail', 'Art & Design', 'Manufacturing', 'Media'],
+        datasets: [
+          {
+            label: 'Clients in this Industry',
+            data: [technology,hospitality,finance,retail,artAndDesign,manufacturing,media],
+            backgroundColor:"#88B2D8",
+          },
+          {
+            label: 'Dataset 2',
+            data: [5, 7, 4, 7, 8, 2, 4],
+            backgroundColor: "#9CCC89"
+          },
+          {
+            label: 'Dataset 3',
+            data: [5, 7, 4, 7, 8, 2, 4],
+            backgroundColor: "#9CCC34"
+          }
+        ],
+      };
 
 
     return (
         <div>
             <h3>Most Interacted</h3>
-            {/* <Bar data={data} options={options}/> */}
+            <Bar data={data} options={options}/>
         </div>
 
     )
