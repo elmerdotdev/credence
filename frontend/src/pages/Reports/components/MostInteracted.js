7b8ae9618c821af8db3e232088d9d19d06af7579
Danika Enad <danika.enad@gmail.com>
2022-11-22 22:38:47 -0800

    updated graphs to be more get userID

--- frontend/src/pages/Reports/components/MostInteracted.js
+++ frontend/src/pages/Reports/components/MostInteracted.js
@@ -17,56 +17,56 @@ const MostInteracted = () => {
     const [dateCreated, setDateCreated] = useState('')
     const [phone, setPhone] = useState('')
 
-    useEffect(() => {
+    // useEffect(() => {
 
 
-        const getNotes = async () => {
-            const notes = await fetchNotes();
-            // const notesByClient = notes.reduce((map, val) => {
-            //     if(!map[val.client_id]) {
-            //         map[val.client_id] = [];
-            //     }
-            //     map[val.client_id].push(val.client_id);
-            //     return map
-            // })
-            const notesByClient = notes.map()
-            setNotes(notes);
-            console.log(notesByClient)
-        }
+    //     const getNotes = async () => {
+    //         const notes = await fetchNotes();
+    //         // const notesByClient = notes.reduce((map, val) => {
+    //         //     if(!map[val.client_id]) {
+    //         //         map[val.client_id] = [];
+    //         //     }
+    //         //     map[val.client_id].push(val.client_id);
+    //         //     return map
+    //         // })
+    //         const notesByClient = notes.map()
+    //         setNotes(notes);
+    //         console.log(notesByClient)
+    //     }
 
-        const getEvents = async () => {
-            const events = await fetchEvents();
-            setEvents(events);
-            console.log(events);
-        }
+    //     const getEvents = async () => {
+    //         const events = await fetchEvents();
+    //         setEvents(events);
+    //         console.log(events);
+    //     }
 
-        getNotes();
-        getEvents();
-    }, [])
+    //     getNotes();
+    //     getEvents();
+    // }, [])
 
 
-    const fetchNotes = async () => {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/notes/63645e4850049bfd1e89637a`)
-        const data = await response.json()
+    // const fetchNotes = async () => {
+    //     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/notes/63645e4850049bfd1e89637a`)
+    //     const data = await response.json()
 
-        if (response.ok) {
-            return data
-        }
-    }
+    //     if (response.ok) {
+    //         return data
+    //     }
+    // }
 
-    const fetchEvents = async () => {
-        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/activities/63645e4850049bfd1e89637a`)
-        const data = await response.json()
+    // const fetchEvents = async () => {
+    //     const response = await fetch(`${process.env.REACT_APP_API_URL}/api/activities/63645e4850049bfd1e89637a`)
+    //     const data = await response.json()
 
-        if (response.ok) {
-            return data
-        }
-    }
+    //     if (response.ok) {
+    //         return data
+    //     }
+    // }
 
-    const xArray = dateCreated;
-    const yArray = phone - 1000;
-    const rArray = ["5", "6", "7", "9", "5", "5", "7", "10", "4", "3", "5", "6", "7", "9", "5", "5", "7", "10", "4", "3"]
-    const chartData = [];
+    // const xArray = dateCreated;
+    // const yArray = phone - 1000;
+    // const rArray = ["5", "6", "7", "9", "5", "5", "7", "10", "4", "3", "5", "6", "7", "9", "5", "5", "7", "10", "4", "3"]
+    // const chartData = [];
 
     // xArray.forEach(function(e, i) {
     //     chartData.push({
@@ -76,27 +76,27 @@ const MostInteracted = () => {
     //     });
     // });
 
-    const data = {
-        datasets: [{
-          label: 'First Dataset',
-          data: chartData,
-          backgroundColor: 'rgb(255, 99, 132)'
-        }]
-      };
+    // const data = {
+    //     datasets: [{
+    //       label: 'First Dataset',
+    //       data: chartData,
+    //       backgroundColor: 'rgb(255, 99, 132)'
+    //     }]
+    //   };
 
-    const options = {
-        plugins: {
-            legend: {
-                display: false,
-            }
-        }
-    }
+    // const options = {
+    //     plugins: {
+    //         legend: {
+    //             display: false,
+    //         }
+    //     }
+    // }
 
 
     return (
         <div>
             <h3>Most Interacted</h3>
-            <Bar data={data} options={options}/>
+            {/* <Bar data={data} options={options}/> */}
         </div>
 
     )
