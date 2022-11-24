17a35ac0aba2767e29ff2054ea1585be5f6d5f0a
Iris Chen <jaminitaa0322@gmail.com>
2022-11-24 11:46:03 -0800

    update gmail secret files path for render deployment

--- backend/controllers/gmailAuthModule/gmail.js
+++ backend/controllers/gmailAuthModule/gmail.js
@@ -10,8 +10,8 @@ const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const TOKEN_PATH = path.join('/etc/secrets/token.json');
+const CREDENTIALS_PATH = path.join('/etc/secrets/credentials.json');
 
 /**
  * Reads previously authorized credentials from the save file.
