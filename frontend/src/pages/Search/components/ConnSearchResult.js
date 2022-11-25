a79c857df0214dc59685a7bb6394246dd0d40904
Elmer <contact@elmer.dev>
2022-11-25 01:45:28 -0800

    dark mode magic

--- frontend/src/pages/Search/components/ConnSearchResult.js
+++ frontend/src/pages/Search/components/ConnSearchResult.js
@@ -3,12 +3,24 @@
 const ConnSearchResult = ({ connection, onToggleConn }) => {
     return (
       <div className="search-single" onClick = {() => onToggleConn(connection._id)}>
-        <div >
-        <h3>
-        {connection.firstname}{' '}
-        {connection.lastname}{' '}
-        </h3>
-        </div>
+        {connection.photo ?
+          <div className="connection-result connection-with-photo">
+            <div className="connection-result-photo-wrapper">
+              <img src={connection.photo} alt={connection.firstname} />
+            </div>
+            <h3>
+              {connection.firstname}{' '}
+              {connection.lastname}{' '}
+            </h3>
+          </div>
+        :
+          <div className="connection-result">
+            <h3>
+              {connection.firstname}{' '}
+              {connection.lastname}{' '}
+            </h3>
+          </div>
+        }
         <div className="search-type-button-wrapper"><button className="search-type-button">Connection</button></div>
   
       </div>
